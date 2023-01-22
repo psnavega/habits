0e7787d932a6473cf0a0330baf52998227edd1a8
Patrick Navega <psnavega97@icloud.com>
2023-01-22 02:49:49 -0300

    feat: create habit day component

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,32 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { Header } from "../components/Header";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+
+
+const weekDays = ['D','S','T','Q','Q','S','S'];
+
 
 export function Home() {
     return (
         <View className="flex-1 bg-background px-8 pt-16">
-            <Header></Header>
+            <Header />
+
+            <View>
+                {
+                    weekDays.map((weekDay, index) => {
+                        return (
+                            <Text 
+                                key={`${weekDay}-${index}`}
+                                className="text-zinc-400 text-xl font-bold text-center mx-1"
+                                style={{ width: DAY_SIZE}}>
+                                {weekDay}
+                            </Text>
+                        )
+                    })
+                }
+            </View>
+            
+            <HabitDay/>
         </View>
     )
 }
