c80d95e519eb654636481de1cc709f09f118f6e8
Patrick Navega <psnavega97@icloud.com>
2023-01-22 15:18:19 -0300

    feat: create habit input

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,11 +1,28 @@
-import { ScrollView, View } from "react-native"
+import { ScrollView, View, Text, TextInput } from "react-native"
 import { BackButton } from "../components/BackButton";
 
 export function New() {
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <ScrollView showsVerticalScrollIndicator={false}>
+                
                 <BackButton />
+                
+                <Text
+                    className="mt-6 text-white font-extrabold text-3xl"
+                >
+                    Criar hábito
+                </Text>
+                
+                <Text
+                    className="mt-6 text-white font-semibold text-base"
+                >
+                    Qual o seu comprometimento?
+                </Text>
+
+                <TextInput
+                    className="h-12 pl-4 rounded-lg mt-3 bg-zinc-300 text-white focus:border-2 focus:border-green-600"
+                />
             </ScrollView>
         </View>
     );
