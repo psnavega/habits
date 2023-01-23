a5f68b36dbdf414c0422996777de6736094d4cfc
Patrick Navega <psnavega97@icloud.com>
2023-01-22 23:08:30 -0300

    feat: create check box

--- client/src/components/HabitDay.tsx
+++ client/src/components/HabitDay.tsx
@@ -1,6 +1,8 @@
 import * as Popover from '@radix-ui/react-popover'
 import clsx from 'clsx'
+import * as CheckBox from '@radix-ui/react-checkbox'
 import { ProgressBar } from './ProgressBar'
+import { Check } from 'phosphor-react'
 
 interface HabitDayProps {
     completed: number
@@ -30,6 +32,22 @@ export function HabitDay({completed, amount} : HabitDayProps) {
 
                         <ProgressBar progress={completedPercentage}/>
 
+                        <div className='mt-6 flex flex-col gap-3'>
+                            <CheckBox.Root
+                                className='flex items-center gap-3 group'
+                            >
+                                <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                                    <CheckBox.Indicator>
+                                        <Check size={20} className='text-white'/>
+                                    </CheckBox.Indicator>
+                                </div>
+
+                                <span className='font-semibold text-xl text-white leading-tight'>
+                                    Segunda-feira
+                                </span>
+                            </CheckBox.Root>
+
+                        </div>
                         <Popover.Arrow className='fill-zinc-900'/>
                     </Popover.Content>
                 </Popover.Portal>
--- client/src/components/newHabitForm.tsx
+++ client/src/components/newHabitForm.tsx
@@ -1,4 +1,15 @@
-import { Check } from "phosphor-react";
+import { Check } from "phosphor-react"
+import * as CheckBox from '@radix-ui/react-checkbox'
+
+const availableWeekDays = [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado',
+]
 
 export function NewHabitForm() {
     return (
@@ -17,10 +28,31 @@ export function NewHabitForm() {
                 Qual a recorrência?
             </label>
 
+            <div className="flex flex-col gap-2 mt-3">
+                {availableWeekDays.map(weekDay => (
+                    <CheckBox.Root
+                        key={weekDay}
+                        className='flex items-center gap-3 group'
+                    >
+                        <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                            <CheckBox.Indicator>
+                                <Check size={20} className='text-white'/>
+                            </CheckBox.Indicator>
+                        </div>
+
+                        <span className='text-white leading-tight'>
+                            {weekDay}
+                        </span>
+                    </CheckBox.Root>
+                ))}
+            </div>
+
             <button type='submit' className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
                 <Check size={20} weight='bold'/>
                 Confirmar
             </button>
+
+            
         </form>
     )
 }
